c529a0af4eef52fe41fb63458ec72537bbff10b1
Tcnh <tom.heintzberger@gmail.com>
2021-06-25 16:46:21 +0200

    Add defaultIssueTypes

---
+++ src/Orangebeard.Client/Abstractions/Models/DefaultIssueType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orangebeard.Client.Abstractions.Models
+{
+    public struct DefaultIssueType
+    {
+        public const string ProductBug = "PB001";
+        public const string AutomationBug = "AB001";
+        public const string SystemIssue = "SI001";
+        public const string ToInvestigate = "TI001";
+        public const string NotDefect = "ND001";
+    }
+}
