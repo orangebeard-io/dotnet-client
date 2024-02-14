1b047dbdec88e89e2efa8532af62a8fbaa0f5722
tcnh <tom@orangebeard.io>
2024-02-14 10:52:29 +0100

    Git add log - entity should not be ignored

---
+++ src/Orangebeard.Client/V3/Entity/Log/Log.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Orangebeard.Client.V3.Entity.Log
+{
+    public class Log
+    {
+        [JsonProperty("testRunUUID")]
+        public Guid TestRunUUID { get; set; }
+        [JsonProperty("testUUID")]
+        public Guid TestUUID { get; set; }
+        [JsonProperty("stepUUID")]
+        public Guid? StepUUID { get; set; }
+        [JsonProperty("message")]
+        public string Message { get; set; }
+        [JsonProperty("logLevel")]
+        public LogLevel LogLevel { get; set; }
+        [JsonProperty("logTime")]
+        public DateTime LogTime { get; set; }
+        [JsonProperty("logFormat")]
+        public LogFormat LogFormat { get; set; }
+    }
+}
