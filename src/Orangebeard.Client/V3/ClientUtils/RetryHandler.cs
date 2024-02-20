70387dbdda2e271fc726646620950860c713fd15
tcnh <tom@orangebeard.io>
2024-02-20 16:52:51 +0100

    Refactored Errorhandling and retry mechanism

--- src/Orangebeard.Client/V3/ClientUtils/RetryHandler.cs
+++ src/Orangebeard.Client/V3/ClientUtils/RetryHandler.cs
@@ -2,23 +2,25 @@
 using Polly.Retry;
 using System;
 using System.Net.Http;
+using Orangebeard.Client.V3.ClientUtils.Logging;
 
 namespace Orangebeard.Client.V3.ClientUtils
 {
     internal class RetryHandler : DelegatingHandler
     {
-        private readonly AsyncRetryPolicy<HttpResponseMessage> _retryPolicy;
+        private static readonly ILogger Logger = LogManager.Instance.GetLogger<RetryHandler>();
+        private readonly AsyncRetryPolicy _retryPolicy;
 
-        public RetryHandler(HttpMessageHandler innerHandler, int maxRetries = 3)
+        public RetryHandler(HttpMessageHandler innerHandler, int maxRetries = 4)
             : base(innerHandler)
         {
             _retryPolicy = Policy
                 .Handle<HttpRequestException>()
                 .Or<System.Net.Sockets.SocketException>()
-                .OrResult<HttpResponseMessage>(response => !response.IsSuccessStatusCode)
+                .Or<Exception>()
                 .WaitAndRetryAsync(maxRetries, retryAttempt =>
                 {
-                    Console.WriteLine("Retry attempt {0}/3", retryAttempt);
+                    Logger.Warn($"[ORANGEBEARD] Retry attempt {retryAttempt}/{maxRetries}");
                     return TimeSpan.FromSeconds(Math.Pow(2, retryAttempt));
                 });
         }
@@ -28,8 +30,12 @@ namespace Orangebeard.Client.V3.ClientUtils
             System.Threading.CancellationToken cancellationToken)
         {
             return await _retryPolicy.ExecuteAsync(
-                async () => await base.SendAsync(request, cancellationToken)
-            );
+                async () =>
+                {
+                    var response = await base.SendAsync(request, cancellationToken);
+                    response.EnsureSuccessStatusCode();
+                    return response;
+                });
         }
     }
 }
